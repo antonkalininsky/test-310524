3a628a67fc155770977c75ec32af92a4bec52504
anton <chadik.free@gmail.com>
2024-05-31 14:50:38 +0500

    app layout

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,41 @@
 <script setup lang="ts">
 import ButtonComponent from '@/components/UI/ButtonComponent.vue'
+import InputComponent from '@/components/UI/InputComponent.vue'
+import TaskItem from '@/components/TaskItem.vue'
 
 </script>
 
 <template>
-  <div>
-    <ButtonComponent>
-      кнопка
-    </ButtonComponent>
+  <div class="task-list">
+    <div class="task-list__control">
+      <InputComponent />
+      <ButtonComponent>
+        добавить задачу
+      </ButtonComponent>
+    </div>
+    <div class="task-list__items">
+      <TaskItem />
+      <TaskItem />
+      <TaskItem />
+      <TaskItem />
+      <TaskItem />
+      <TaskItem />
+    </div>
   </div>
 </template>
 
 <style scoped lang="scss">
+.task-list {
+  display: grid;
+  row-gap: 18px;
+  &__control {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  &__items {
+    display: grid;
+    row-gap: 4px;
+  }
+}
 </style>
