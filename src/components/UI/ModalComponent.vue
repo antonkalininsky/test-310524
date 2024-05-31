efb488a9a1ad4372ac1a6f7c6a262690a2bf8bab
anton <chadik.free@gmail.com>
2024-05-31 16:44:12 +0500

    modal: working on layout

--- src/components/UI/ModalComponent.vue
+++ src/components/UI/ModalComponent.vue
@@ -1,11 +1,45 @@
 <script setup lang="ts">
+import ButtonComponent from '@/components/UI/ButtonComponent.vue'
+import { ref } from 'vue';
+
+// todo - typing
+const isOpen = ref(false)
+
+function toggle(): void {
+  isOpen.value = !isOpen.value
+}
+
+defineExpose({toggle})
 </script>
 
 <template>
-  <div>
-    modal
+  <div class="modal" v-if="isOpen" @click.stop="isOpen = false">
+    <div class="modal__inner">
+      <div class="modal__control">
+        <ButtonComponent>
+          X
+        </ButtonComponent>
+      </div>
+      <div class="modal__view">
+        <slot></slot>
+      </div>
+    </div>
   </div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+.modal {
+  display: grid;
+  justify-content: center;
+  align-items: center;
+  background-color: black;
+  opacity: 0.5;
+  .modal__inner {
+    max-width: 600px;
+    min-width: 400px;
+    padding: 2rem;
+    border-radius: 5px;
+    background-color: white;
+  }
+}
 </style>
