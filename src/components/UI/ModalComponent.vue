24ee0366399038269b843fa4c97d1af7fd9f1cc9
anton <chadik.free@gmail.com>
2024-06-01 10:41:52 +0500

    modalComponent: layout

--- src/components/UI/ModalComponent.vue
+++ src/components/UI/ModalComponent.vue
@@ -1,45 +1,64 @@
 <script setup lang="ts">
 import ButtonComponent from '@/components/UI/ButtonComponent.vue'
-import { ref } from 'vue';
+import { ref } from 'vue'
 
 // todo - typing
-const isOpen = ref(false)
+const isOpen = ref(true)
 
 function toggle(): void {
   isOpen.value = !isOpen.value
 }
 
-defineExpose({toggle})
+defineExpose({ toggle })
 </script>
 
 <template>
-  <div class="modal" v-if="isOpen" @click.stop="isOpen = false">
+  <div class="modal" v-if="isOpen">
     <div class="modal__inner">
       <div class="modal__control">
-        <ButtonComponent>
-          X
-        </ButtonComponent>
+        <ButtonComponent @click="isOpen = false"> X </ButtonComponent>
       </div>
       <div class="modal__view">
         <slot></slot>
       </div>
     </div>
+    <div class="modal__overlay" @click.prevent="isOpen = false"></div>
   </div>
 </template>
 
 <style scoped lang="scss">
 .modal {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
   display: grid;
   justify-content: center;
   align-items: center;
-  background-color: black;
-  opacity: 0.5;
-  .modal__inner {
-    max-width: 600px;
-    min-width: 400px;
-    padding: 2rem;
-    border-radius: 5px;
+  &__overlay {
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background-color: black;
+    opacity: 0.5;
+  }
+  &__inner {
+    border-radius: 6px;
     background-color: white;
+    opacity: 1 !important;
+    z-index: 2;
+  }
+  &__control {
+    display: flex;
+    flex-direction: row-reverse;
+    padding: 10px;
+  }
+  &__view {
+    padding-inline: 30px;
+    padding-bottom: 40px;
   }
 }
 </style>
